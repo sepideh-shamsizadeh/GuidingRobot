491d5080dc2fc3a301e12b2d5a4c9522690c15c7
sepideh <sepideh.shamsizadeh@gmail.com>
2022-11-16 17:03:53 +0100

    detection

--- src/auto_labeling/camera.py
+++ src/auto_labeling/camera.py
@@ -21,12 +21,12 @@ import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
 
-from Thesis-guiding-robot.src.yolov7.models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+from src.yolov7.models.experimental import attempt_load
+from src.yolov7.utils.datasets import LoadStreams, LoadImages
+from src.yolov7.utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
-from utils.plots import plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+from src.yolov7.utils.plots import plot_one_box
+from src.yolov7.utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
 
 
 def callback_image(data):
---
+++ src/auto_labeling/detect.py
@@ -0,0 +1,101 @@
+
+import argparse
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+import numpy as np
+
+from src.yolov7.models.experimental import attempt_load
+from src.yolov7.utils.datasets import LoadStreams, LoadImages
+from src.yolov7.utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from src.yolov7.utils.datasets import letterbox
+from src.yolov7.utils.plots import plot_one_box
+from src.yolov7.utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+
+def detect_person(img0):
+    poses = []
+    weights = 'yolov7.pt'
+    imgsz = 640
+    stride = 32
+    conf_thres = 0.5
+    iou_thres = 0.45
+    nosave = False
+    source= 'inference/images'
+    weights='yolov7.pt'
+    view_img= False
+    save_txt= False
+    project = 'runs/detect'
+    name = 'exp'
+    exist_ok = False
+    save_img = not nosave and not source.endswith('.txt')  # save inference images
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+
+    # Directories
+    save_dir = Path(increment_path(Path(project) / name, exist_ok=exist_ok))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Initialize
+    set_logging()
+    device = select_device('')
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+
+    img = letterbox(img0, imgsz, stride=stride)[0]
+
+    # Convert
+    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+    img = np.ascontiguousarray(img)
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    old_img_w = old_img_h = imgsz
+    old_img_b = 1
+
+    t0 = time.time()
+    img = torch.from_numpy(img).to(device)
+    img = img.half() if half else img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+
+    # Warmup
+    if device.type != 'cpu' and (
+            old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+        old_img_b = img.shape[0]
+        old_img_h = img.shape[2]
+        old_img_w = img.shape[3]
+        for i in range(3):
+            model(img, augment='store_true')[0]
+
+    # Inference
+    t1 = time_synchronized()
+    with torch.no_grad():  # Calculating gradients would cause a GPU memory leak
+        pred = model(img, augment='store_true')[0]
+    t2 = time_synchronized()
+
+    # Apply NMS
+    pred = non_max_suppression(pred, conf_thres, iou_thres)
+    t3 = time_synchronized()
+    print(pred)
+    return poses
+
+
+if __name__ == '__main__':
+
+    img0 = cv2.imread("/home/sepideh/Pictures/1.png")  # BGR
+    cv2.imshow("image", img0)
+    cv2.waitKey(0)
+    detect_person(img0)
