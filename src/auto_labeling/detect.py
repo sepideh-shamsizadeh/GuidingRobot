7cd05e81c626a2033929ae4238e8969f8486365f
sepideh <sepideh.shamsizadeh@gmail.com>
2022-11-17 16:06:56 +0100

    detect_person function

--- src/auto_labeling/detect.py
+++ src/auto_labeling/detect.py
@@ -1,44 +1,26 @@
-
-import argparse
-import time
-from pathlib import Path
-import sys
-import cv2
-import torch
-import torch.backends.cudnn as cudnn
-from numpy import random
-import numpy as np
 import os
+import sys
+import time
+
+import cv2
+import numpy as np
+import torch
+from numpy import random
+
 from src.yolov7.models.experimental import attempt_load
-from src.yolov7.utils.datasets import LoadStreams, LoadImages
-from src.yolov7.utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
-    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 from src.yolov7.utils.datasets import letterbox
+from src.yolov7.utils.general import check_img_size, non_max_suppression, scale_coords, xyxy2xywh, set_logging
 from src.yolov7.utils.plots import plot_one_box
-from src.yolov7.utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+from src.yolov7.utils.torch_utils import select_device
 
 
 def detect_person(img0):
     poses = []
     imgsz = 640
-    stride = 32
     conf_thres = 0.5
     iou_thres = 0.45
-    nosave = False
-    source= 'inference/images'
-    weights = 'yolov7.pt'
-    view_img= False
-    save_txt= False
-    project = 'runs/detect'
-    name = 'exp'
-    exist_ok = False
-    save_img = not nosave and not source.endswith('.txt')  # save inference images
-    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-        ('rtsp://', 'rtmp://', 'http://', 'https://'))
 
-    # Directories
-    save_dir = Path(increment_path(Path(project) / name, exist_ok=exist_ok))  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+    weights = 'yolov7.pt'
 
     # Initialize
     set_logging()
@@ -71,25 +53,12 @@ def detect_person(img0):
     if img.ndimension() == 3:
         img = img.unsqueeze(0)
 
-    # Warmup
-    if device.type != 'cpu' and (
-            old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
-        old_img_b = img.shape[0]
-        old_img_h = img.shape[2]
-        old_img_w = img.shape[3]
-        for i in range(3):
-            model(img, augment='store_true')[0]
-
     # Inference
-    t1 = time_synchronized()
     with torch.no_grad():  # Calculating gradients would cause a GPU memory leak
         pred = model(img, augment='store_true')[0]
-    t2 = time_synchronized()
 
     # Apply NMS
     pred = non_max_suppression(pred, conf_thres, iou_thres)
-    t3 = time_synchronized()
-    print(pred)
     for i, det in enumerate(pred):
         gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
         if len(det):
@@ -100,19 +69,14 @@ def detect_person(img0):
             for c in det[:, -1].unique():
                 n = (det[:, -1] == c).sum()  # detections per class
                 s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-                print(s)
             # Write results
             for *xyxy, conf, cls in reversed(det):
-                xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                line = (cls, *xywh)  # label format
-
-                if save_img or view_img:  # Add bbox to image
-                    label = f'{names[int(cls)]} {conf:.2f}'
+                label = f'{names[int(cls)]} {conf:.2f}'
+                if 'person' in label:
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                    line = (cls, *xywh)  # label format
                     plot_one_box(xyxy, img0, label=label, color=colors[int(cls)], line_thickness=1)
-                    print('label',label)
-
-        # Print time (inference + NMS)
-        print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
+                    poses.append(xywh)
 
     # Stream results
     cv2.imshow('str(p)', img0)
@@ -122,8 +86,8 @@ def detect_person(img0):
 
 
 if __name__ == '__main__':
-
     img0 = cv2.imread("/home/sepideh/Pictures/1.png")  # BGR
     cv2.imshow("image", img0)
     cv2.waitKey(0)
-    detect_person(img0)
+    p = detect_person(img0)
+    print(p)
