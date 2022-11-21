aec0d1e74049d6a63b6317e05365abf8be098fc9
sepideh <sepideh.shamsizadeh@gmail.com>
2022-11-21 14:09:45 +0100

    detection with gpu

--- src/auto_labeling/detect.py
+++ src/auto_labeling/detect.py
@@ -8,38 +8,52 @@ import torch
 from numpy import random
 
 from src.yolov7.models.experimental import attempt_load
-from src.yolov7.utils.datasets import letterbox
+from src.yolov7.utils.datasets import letterbox, LoadImages
 from src.yolov7.utils.general import check_img_size, non_max_suppression, scale_coords, xyxy2xywh, set_logging, xyn2xy
 from src.yolov7.utils.plots import plot_one_box
-from src.yolov7.utils.torch_utils import select_device
+from src.yolov7.utils.torch_utils import select_device, time_synchronized, load_classifier, TracedModel
 
 
-def detect_person(img0):
+def detect_person(source):
     poses = []
     imgsz = 640
     conf_thres = 0.5
     iou_thres = 0.45
-
+    here = os.path.dirname(os.path.abspath('yolov7'))
+    sys.path.append(here)
     weights = 'yolov7.pt'
 
     # Initialize
     set_logging()
     device = select_device('')
     half = device.type != 'cpu'  # half precision only supported on CUDA
-    here = os.path.dirname(os.path.abspath('yolov7'))
-    sys.path.append(here)
+
     # Load model
     model = attempt_load(weights, map_location=device)  # load FP32 model
     stride = int(model.stride.max())  # model stride
     imgsz = check_img_size(imgsz, s=stride)  # check img_size
-    s = ''
-    img = letterbox(img0, imgsz, stride=stride)[0]
+    trace = True
+    if trace:
+        model = TracedModel(model, device, imgsz)
 
-    # Convert
-    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
-    img = np.ascontiguousarray(img)
+    if half:
+        model.half()  # to FP16
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+
+    dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
     # Run inference
     if device.type != 'cpu':
         model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
@@ -47,47 +61,69 @@ def detect_person(img0):
     old_img_b = 1
 
     t0 = time.time()
-    img = torch.from_numpy(img).to(device)
-    img = img.half() if half else img.float()  # uint8 to fp16/32
-    img /= 255.0  # 0 - 255 to 0.0 - 1.0
-    if img.ndimension() == 3:
-        img = img.unsqueeze(0)
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
 
-    # Inference
-    with torch.no_grad():  # Calculating gradients would cause a GPU memory leak
-        pred = model(img, augment='store_true')[0]
+        # Warmup
+        if device.type != 'cpu' and (
+                old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+            old_img_b = img.shape[0]
+            old_img_h = img.shape[2]
+            old_img_w = img.shape[3]
+            for i in range(3):
+                model(img, augment=False)[0]
 
-    # Apply NMS
-    pred = non_max_suppression(pred, conf_thres, iou_thres)
-    for i, det in enumerate(pred):
-        gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-        if len(det):
-            # Rescale boxes from img_size to im0 size
-            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+        # Inference
+        t1 = time_synchronized()
+        with torch.no_grad():  # Calculating gradients would cause a GPU memory leak
+            pred = model(img, augment=False)[0]
+        t2 = time_synchronized()
 
-            # Print results
-            for c in det[:, -1].unique():
-                n = (det[:, -1] == c).sum()  # detections per class
-                s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-            # Write results
-            for *xyxy, conf, cls in reversed(det):
-                label = f'{names[int(cls)]} {conf:.2f}'
-                if 'person' in label:
-                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4))).view(-1).tolist()  # normalized xywh
+        # Apply NMS
+        pred = non_max_suppression(pred, conf_thres, iou_thres)
+        t3 = time_synchronized()
+
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                     line = (cls, *xywh)  # label format
-                    plot_one_box(xyxy, img0, label=label, color=colors[int(cls)], line_thickness=1)
+                    label = f'{names[int(cls)]} {conf:.2f}'
+                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
                     poses.append(xywh)
 
-    # Stream results
-    cv2.imshow('str(p)', img0)
-    cv2.waitKey(0)  # 1 millisecond
+            # Print time (inference + NMS)
+            print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
+
+            # Stream results
+
+            cv2.imshow(str(p), im0)
+            cv2.waitKey(1)  # 1 millisecond
 
     return poses
 
 
 if __name__ == '__main__':
-    img0 = cv2.imread("/home/sepideh/Pictures/2.png")  # BGR
+    source = "/home/ubuntu20-sep/Pictures/"
+    img0 = cv2.imread(source+'1.png')  # BGR
     cv2.imshow("image", img0)
     cv2.waitKey(0)
-    p = detect_person(img0)
+    p = detect_person(source)
     print(p)
--- src/auto_labeling/ros_mathImage2Laser.py
+++ src/auto_labeling/ros_mathImage2Laser.py
@@ -1,5 +1,5 @@
 import rospy
-from sensor_ros.msg import Image
+
 from cv_bridge import CvBridge, CvBridgeError
 import detect
 from geometry_msgs.msg import PoseArray, Pose
@@ -17,12 +17,14 @@ def callback_image(data):
         return
     pose_image.append(detect.detect_person(cv_image))
 
+
 def callback(poses):
     pose = Pose()
+    print(poses)
 
 
 if __name__ == '__main__':
     rospy.loginfo('start matching')
     rospy.init_node('Matcher', anonymous=True, log_level=rospy.INFO)
-    rospy.Subscriber('/theta_camera/image_raw', Image, callback_image, queue_size=2, buff_size=2**24)
+    rospy.Subscriber('/theta_camera/image_raw', callback_image, queue_size=2, buff_size=2**24)
     rospy.Subscriber('/dr_spamm_detection', PoseArray, callback, queue_size=2)
