6a6cc503cde08edb2d4b8831ccd9d1b6b9dd97d0
sepideh <sepideh.shamsizadeh@gmail.com>
2022-11-29 15:41:42 +0100

    save data from related topic

--- src/auto_labeling/ros_mathImage2Laser.py
+++ src/auto_labeling/ros_mathImage2Laser.py
@@ -8,10 +8,23 @@ from sensor_msgs.msg import Image
 
 pose_image = []
 pose_laser = []
+model = load_model()
 
 
 def callback_image(data):
-    pose_image.append(data)
+    try:
+        cv_bridge = CvBridge()
+        cv_image = cv_bridge.imgmsg_to_cv2(data, "bgr8")
+        cv2.imwrite('../images/'+str(len(pose_image))+'.png', cv_image)
+        detected = detect_person(cv_image, model)
+        rospy.Subscriber('/dr_spaam_detections', PoseArray, callback, queue_size=2)
+        pose_image.append(detected)
+        with open('im_pose.txt', 'a') as f:
+            f.write(" ".join(str(item) for item in detected))
+            f.write('\n')
+    except CvBridgeError as e:
+        rospy.logerr('Converting Image Error.' + str(e))
+        return
 
 
 def callback(poses):
@@ -24,8 +37,7 @@ def callback(poses):
 if __name__ == '__main__':
     rospy.loginfo('start matching')
     rospy.init_node('Matcher', anonymous=True, log_level=rospy.INFO)
-    rospy.Subscriber('/theta_camera/image_raw', Image, callback_image, queue_size=2)
-    rospy.Subscriber('/dr_spaam_detections', PoseArray, callback, queue_size=2)
+    rospy.Subscriber('/theta_camera/image_raw', Image, callback_image, queue_size=2, buff_size=2**24)
     rospy.spin()
     print(len(pose_laser), len(pose_image))
 
