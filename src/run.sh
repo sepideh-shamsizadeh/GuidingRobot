e874abffb400fc54a748505c4288fe81448d3088
Sepideh <sepideh.shamsizadeh@gmail.com>
2023-05-17 21:52:56 +0200

    sides H matrix

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the path to the folder of images
-IMAGE_DIR="auto_labeling/calib/scenes/back"
+IMAGE_DIR="auto_labeling/calib/scenes/left"
 
 # Loop through all the files in the folder
 for IMAGE_FILE in "$IMAGE_DIR"/*; do
